743cbb1d5d063f5aeb974a1e89bf9dbc3ebe6749
CSchnitzerling <110841@student.sae.edu.au>
2015-10-18 00:25:26 +1000

    Created Test Scene and first enemy

---
+++ Age of Anubis/Assets/Scripts/Entities/Enemies/ENY_Scarab_001.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ENY_Scarab_001 : Enemy 
+{
+	[Header("ScarabStats")]
+	public float m_moveSpeed = 10f;
+	public float m_fakeGravity = 10f;
+
+	private float m_localGravityScale;
+
+	public override void AwakeOverride()
+	{
+		base.AwakeOverride();
+
+		m_localGravityScale = m_rb.gravityScale;
+	}
+
+
+	public override void EnemyBehaviour()
+	{
+
+		CheckReturn checkreturn = CheckEnemyLocation();
+
+		if (checkreturn.colBelow != null)
+		{
+			m_rb.gravityScale = 0;
+
+			m_rb.velocity = transform.right * m_moveSpeed;
+
+			m_rb.AddRelativeForce(Vector3.down * m_fakeGravity);
+		}
+		else
+		{
+			m_rb.gravityScale = m_localGravityScale;
+		}
+
+		if (checkreturn.type == CheckReturnEnum.ReachedWall || checkreturn.type == CheckReturnEnum.ReachedEdge)
+		{
+			transform.rotation = transform.rotation * Quaternion.FromToRotation(Vector2.left, Vector2.right);
+		}
+
+
+	}
+
+
+
+}
--- Age of Anubis/Assets/Scripts/Entities/Enemies/Enemy.cs
+++ Age of Anubis/Assets/Scripts/Entities/Enemies/Enemy.cs
@@ -6,7 +6,31 @@ public class Enemy : Damageable
 {
 	[Header("EnemyStats")]
 	public Attack m_attack;
+	private BoxCollider2D m_colBox;
+	private CircleCollider2D m_colCircle;
 
+
+	private BoxCollider2D m_forwardCol;
+	private BoxCollider2D m_belowCol;
+	private BoxCollider2D m_cornerCol;
+
+	protected enum CheckReturnEnum { None, Grounded, ReachedEdge, ReachedWall };
+
+	protected struct CheckReturn
+	{
+		public CheckReturnEnum type;
+		public Collider2D colFront;
+		public Collider2D colBelow;
+	};
+
+
+	public override void AwakeOverride()
+	{
+		m_colBox = GetComponent<BoxCollider2D>();
+		m_colCircle = GetComponent<CircleCollider2D>();
+
+		CreateDirectionColliders();
+	}
 
 	// Update is called once per frame
 	public override void UpdateOverride()
@@ -17,7 +41,21 @@ public class Enemy : Damageable
 	public virtual void EnemyBehaviour()
 	{
 
+		if (CheckEnemyLocation().type == CheckReturnEnum.None)
+		{
+			print("Airborne");
+		}
+		else if (CheckEnemyLocation().type == CheckReturnEnum.ReachedEdge)
+		{
+			print("ReachedEdge");
+		}
+		else if (CheckEnemyLocation().type == CheckReturnEnum.ReachedWall)
+		{
+			print("ReachedWall");
+		}
+
 	}
+
 
 	public override void OnDeath()
 	{
@@ -33,5 +71,114 @@ public class Enemy : Damageable
 		}
 	}
 
+	void CreateDirectionColliders()
+	{
+		float offset = 0.05f;
+		float depth = 0.4f;
+
+		m_forwardCol = gameObject.AddComponent<BoxCollider2D>();
+		m_belowCol = gameObject.AddComponent<BoxCollider2D>();
+		m_cornerCol = gameObject.AddComponent<BoxCollider2D>();
+
+		m_forwardCol.isTrigger = true;
+		m_belowCol.isTrigger = true;
+		m_cornerCol.isTrigger = true;
+
+		if (m_colBox != null)
+		{
+			//print("Created From Box");
+			m_forwardCol.size = new Vector2(offset * 2, m_colBox.size.y - offset);
+			m_belowCol.size = new Vector2(m_colBox.size.x - offset, depth);
+			m_cornerCol.size = new Vector2(depth,depth);
+
+			m_forwardCol.offset = new Vector2(m_colBox.offset.x + (m_colBox.size.x / 2) + (offset), m_colBox.offset.y);
+			m_belowCol.offset = new Vector2(m_colBox.offset.x, m_colBox.offset.y - (m_colBox.size.y / 2) - offset - (depth / 2));
+			m_cornerCol.offset = new Vector2(m_colBox.offset.x + (m_colBox.size.x / 2) + offset + (depth / 2), m_colBox.offset.y - (m_colBox.size.y / 2) - offset - (depth / 2));
+
+		}
+		else if (m_colCircle != null)
+		{
+			//print("Created From Circle");
+			m_forwardCol.size = new Vector2(depth, m_colCircle.radius * 2 - offset);
+			m_belowCol.size = new Vector2(m_colCircle.radius * 2 - offset, depth);
+			m_cornerCol.size = new Vector2(depth, depth);
+
+			m_forwardCol.offset = new Vector2(m_colCircle.offset.x + m_colCircle.radius + offset + (depth / 2), m_colCircle.offset.y);
+			m_belowCol.offset = new Vector2(m_colCircle.offset.x, m_colCircle.offset.y - m_colCircle.radius - offset - (depth / 2));
+			m_cornerCol.offset = new Vector2(m_colCircle.offset.x + m_colCircle.radius + offset + (depth / 2), m_colCircle.offset.y - m_colCircle.radius - offset - (depth / 2));
+		}
+		else
+		{
+			Debug.LogError("Broken no collider on Enemy: BOX or CIRCLE only");
+		}
+
+	}
+
+	protected CheckReturn CheckEnemyLocation()
+	{
+		
+		
+
+		CheckReturn tempReturn = new CheckReturn();
+		tempReturn.colBelow = null;
+		tempReturn.colFront = null;
+		tempReturn.type = CheckReturnEnum.None;
+
+		Collider2D[] allColls;
+
+		//CheckBelow
+		allColls = Physics2D.OverlapAreaAll(m_belowCol.bounds.min, m_belowCol.bounds.max);
+
+		foreach (var col in allColls)
+		{
+			if (col.gameObject.CompareTag("NotSolid") || col.gameObject.CompareTag("Solid") || col.gameObject.CompareTag("Wall"))
+			{
+				tempReturn.colBelow = col;
+				tempReturn.type = CheckReturnEnum.Grounded;
+				break;
+			}
+		}
+
+		if (tempReturn.colBelow != null)
+		{
+			//Check Front
+			allColls = Physics2D.OverlapAreaAll(m_forwardCol.bounds.min, m_forwardCol.bounds.max);
+
+			foreach (var col in allColls)
+			{
+				if (col.gameObject.CompareTag("NotSolid") || col.gameObject.CompareTag("Solid") || col.gameObject.CompareTag("Wall"))
+				{
+					tempReturn.colFront = col;
+					tempReturn.type = CheckReturnEnum.ReachedWall;
+					break;
+				}
+			}
+
+			if (tempReturn.colFront == null)
+			{
+				//Check Corner
+				allColls = Physics2D.OverlapAreaAll(m_cornerCol.bounds.min, m_cornerCol.bounds.max);
+
+				bool colWithWall = false;
+
+				foreach (var col in allColls)
+				{
+					if (col.gameObject.CompareTag("NotSolid") || col.gameObject.CompareTag("Solid") || col.gameObject.CompareTag("Wall"))
+					{
+						colWithWall = true;
+					}
+				}
+
+				if (!colWithWall)
+				{
+					tempReturn.type = CheckReturnEnum.ReachedEdge;
+				}
+
+			}
+			
+		}
+
+		return tempReturn;
+	}
 
 }
