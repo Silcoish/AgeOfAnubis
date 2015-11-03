f5aec8864d91efbbec170b3ed5b615d1a50282de
Luke Savage <lsava22@hotmail.com>
2015-11-03 15:36:20 +1000

    Eye Enemy
    -Tracks and follows player
    -Added range check so they wont track from anywhere in the level
    -Added laser
    -Still working on shooting logic

--- Age of Anubis/Assets/Scripts/Entities/Enemies/ENY_Eye_001.cs
+++ Age of Anubis/Assets/Scripts/Entities/Enemies/ENY_Eye_001.cs
@@ -9,17 +9,65 @@ public class ENY_Eye_001 : Enemy
     private float m_curTimer = 0;
     public GameObject m_laserProj;
     public float m_shootDelay = 0.5F;
+    public float m_recharge = 3;
+    private float m_rechargeTimer = 0;
     private bool m_isFacingRight = true;
     private bool m_readyToShoot = false;
+    private Transform m_player;
+    private bool m_isTracking = false;
+    public float m_trackingRange = 10;
+    public float m_xDist = 2;
 
     public override void AwakeOverride()
     {
         base.AwakeOverride();
+
+        m_player = GameObject.FindGameObjectWithTag("Player").transform;
+        if (!m_player)
+            Debug.Log("Eye - Failed to find GameObject with 'Player' tag");
     }
 
     public override void EnemyBehaviour()
     {
-        
+        Vector2 dir = m_player.position - transform.position;
+
+        // Attacking
+        if(m_readyToShoot && m_rechargeTimer >= m_recharge)
+        {
+            Debug.Log("Attacking");
+            GameObject tempProj;
+            tempProj = Instantiate(m_laserProj, transform.position, transform.rotation) as GameObject;
+            tempProj.GetComponent<PROJ_Laser>().SetFacing(dir.x);
+            m_readyToShoot = false;
+            m_rechargeTimer = 0;
+            Pause(m_shootDelay);
+        }
+        else if (Mathf.Abs(dir.x) < (m_xDist + 0.5) && Mathf.Abs(dir.y) < 1)
+        {
+            Debug.Log("Ready to Shoot");
+            m_readyToShoot = true;
+            Pause(m_shootDelay);
+        }
+        m_rechargeTimer += Time.deltaTime;
+        //Debug.Log(m_rechargeTimer);
+        Debug.Log(m_readyToShoot);
+        Debug.Log(dir);
+
+        // Movement
+        m_isTracking = (Mathf.Abs(dir.x) < m_trackingRange && Mathf.Abs(dir.y) < m_trackingRange) ? true : false;
+        if(m_isTracking)
+        {
+            if(Mathf.Abs(dir.x) < m_xDist)
+            {
+                dir.x = 0;
+            }
+            m_rb.velocity = dir.normalized * m_moveSpeed;
+        }
+        else
+            m_rb.velocity = Vector2.zero;
+
+        // Animation
+        UpdateAnimationState(m_rb.velocity.x);
     }
 
     // Flip the character object for left/right facing.
---
+++ Age of Anubis/Assets/Scripts/Entities/Enemies/PROJ_Laser.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PROJ_Laser : MonoBehaviour 
+{
+    public Attack m_attack;
+    public float m_duration = 1;
+    private float m_timer = 0;
+    private bool m_isFacingRight = true;
+    private Vector3 m_startScale;
+
+    void Awake()
+    {
+        m_startScale = transform.localScale;
+    }
+
+	void Update () 
+    {
+	    // expand the scale of the x and y over the duration
+        if (m_timer >= m_duration)
+            Destroy(gameObject);
+        else
+        {
+            Vector3 scale = new Vector3(0, 0, 0);
+            scale.y = Mathf.Lerp(m_startScale.y, 1, m_timer);
+            if(m_isFacingRight)
+                scale.x = Mathf.Lerp(m_startScale.x, 1, m_timer);
+            else
+                scale.x = Mathf.Lerp(m_startScale.x, -1, m_timer);
+
+            transform.localScale = scale;
+        }
+
+        m_timer += Time.deltaTime;
+	}
+
+    public void SetFacing(float dir)
+    {
+        if(dir < 0)
+        {
+            m_isFacingRight = false;
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
+            
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            col.gameObject.GetComponent<Damageable>().OnTakeDamage(m_attack.GetDamage(gameObject.transform));
+        }
+
+    }
+}
