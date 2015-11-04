7ad532fd561cf17bc08360c217df51493f5cccb9
CSchnitzerling <110841@student.sae.edu.au>
2015-11-04 21:58:44 +1000

    Redid the Start Menu to use unities UI

---
+++ Age of Anubis/Assets/OverwriteFirstSelected.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+public class OverwriteFirstSelected : MonoBehaviour 
+{
+	public EventSystem m_es;
+
+
+	void Awake()
+	{
+		m_es = gameObject.GetComponent<EventSystem>();
+
+	}
+
+	void Update () 
+	{
+
+		m_es.firstSelectedGameObject = m_es.currentSelectedGameObject;
+	}
+}
---
+++ Age of Anubis/Assets/Scripts/UI/UIButtons.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UIButtons : MonoBehaviour 
+{
+	public static UIButtons Inst;
+
+	List<GameObject> m_previousMenus;
+	public GameObject m_curMenu;
+
+	void Awake()
+	{
+		if (Inst == null)
+			Inst = this;
+		else
+			Destroy(this);
+
+		m_previousMenus = new List<GameObject>();
+	}
+
+	void Update()
+	{
+		if (Input.GetButtonDown("Fire2"))
+		{
+			print("Back");
+			Back();
+		}
+
+	}
+
+
+	public void NewGame()
+	{
+		LoadingManager.Inst.LoadLevel("ShopScene", true);
+	}
+
+
+	public void ChangeMenus(GameObject targetMenu)
+	{
+		targetMenu.SetActive(true);
+		m_curMenu.SetActive(false);
+		m_previousMenus.Add(m_curMenu);
+		m_curMenu = targetMenu;
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+
+	public void Back()
+	{
+		if (m_previousMenus.Count > 0)
+		{
+			m_previousMenus[m_previousMenus.Count - 1].SetActive(true);
+			m_curMenu.SetActive(false);
+			m_curMenu = m_previousMenus[m_previousMenus.Count - 1];
+			m_previousMenus.Remove(m_curMenu);
+		}
+
+	}
+
+
+}
+
+
