fcca60c10078cbb4f65d3b178dc1f4f56f0b63a5
Luke Savage <lsava22@hotmail.com>
2015-11-05 12:38:42 +1000

    Eye Enemy finished
    -Changed behaviour to a state machine
    -Tested

--- Age of Anubis/Assets/Scripts/Entities/Enemies/ENY_Eye_001.cs
+++ Age of Anubis/Assets/Scripts/Entities/Enemies/ENY_Eye_001.cs
@@ -5,18 +5,20 @@ public class ENY_Eye_001 : Enemy
 {
     [Header("EyeStats")]
     public float m_moveSpeed = 2f;
-    public float m_attackTimer = 5;
-    private float m_curTimer = 0;
-    public GameObject m_laserProj;
-    public float m_shootDelay = 0.5F;
-    public float m_recharge = 3;
-    private float m_rechargeTimer = 0;
-    private bool m_isFacingRight = true;
-    private bool m_readyToShoot = false;
-    private Transform m_player;
-    private bool m_isTracking = false;
+
     public float m_trackingRange = 10;
     public float m_xDist = 2;
+    public float m_chargeTimer = 0.5F;
+    public float m_rechargeTimer = 3;
+    private float m_curTimer = 0;
+    private bool m_isTracking = false;
+    public GameObject m_laserProj;
+    
+    private bool m_isFacingRight = true;
+    private Transform m_player;
+    
+    enum State { SEARCHING, CHARGING, ATTACKING, RECHARGING };
+    private State m_state = State.SEARCHING;
 
     public override void AwakeOverride()
     {
@@ -30,41 +32,56 @@ public class ENY_Eye_001 : Enemy
     public override void EnemyBehaviour()
     {
         Vector2 dir = m_player.position - transform.position;
+        m_rb.velocity = Vector2.zero;
 
-        // Attacking
-        if(m_readyToShoot && m_rechargeTimer >= m_recharge)
+        switch(m_state)
         {
-            Debug.Log("Attacking");
-            GameObject tempProj;
-            tempProj = Instantiate(m_laserProj, transform.position, transform.rotation) as GameObject;
-            tempProj.GetComponent<PROJ_Laser>().SetFacing(dir.x);
-            m_readyToShoot = false;
-            m_rechargeTimer = 0;
-            Pause(m_shootDelay);
-        }
-        else if (Mathf.Abs(dir.x) < (m_xDist + 0.5) && Mathf.Abs(dir.y) < 1)
-        {
-            Debug.Log("Ready to Shoot");
-            m_readyToShoot = true;
-            Pause(m_shootDelay);
-        }
-        m_rechargeTimer += Time.deltaTime;
-        //Debug.Log(m_rechargeTimer);
-        Debug.Log(m_readyToShoot);
-        Debug.Log(dir);
+            case State.SEARCHING:
+                m_isTracking = (Mathf.Abs(dir.x) < m_trackingRange && Mathf.Abs(dir.y) < m_trackingRange) ? true : false;
+                if (m_isTracking)
+                {
+                    if (Mathf.Abs(dir.x) < m_xDist)
+                    {
+                        dir.x = 0;
+                    }
+                    m_rb.velocity = dir.normalized * m_moveSpeed;
+                }
 
-        // Movement
-        m_isTracking = (Mathf.Abs(dir.x) < m_trackingRange && Mathf.Abs(dir.y) < m_trackingRange) ? true : false;
-        if(m_isTracking)
-        {
-            if(Mathf.Abs(dir.x) < m_xDist)
-            {
-                dir.x = 0;
-            }
-            m_rb.velocity = dir.normalized * m_moveSpeed;
+                if (Mathf.Abs(dir.x) < (m_xDist + 0.5) && Mathf.Abs(dir.y) < 1)
+                {
+                    m_state = State.CHARGING;
+                    m_curTimer = m_chargeTimer;
+                    Debug.Log("Enter Charge State");
+                }
+                break;
+            case State.CHARGING:
+                if (m_curTimer <= 0)
+                {
+                    m_state = State.ATTACKING;
+                    Debug.Log("Enter Attack State");
+                }
+                break;
+            case State.ATTACKING:
+                GameObject tempProj;
+                tempProj = Instantiate(m_laserProj, transform.position, transform.rotation) as GameObject;
+                tempProj.GetComponent<PROJ_Laser>().SetFacing(dir.x);
+
+                m_state = State.RECHARGING;
+                m_curTimer = m_rechargeTimer;
+                Debug.Log("Enter Recharge State");
+                break;
+            case State.RECHARGING:
+                if (m_curTimer <= 0)
+                {
+                    m_state = State.SEARCHING;
+                    Debug.Log("Enter Searching State");
+                }
+                break;
+            default:
+                Debug.Log("Eye enemy failed to select valid State");
+                break;
         }
-        else
-            m_rb.velocity = Vector2.zero;
+        m_curTimer -= Time.deltaTime;
 
         // Animation
         UpdateAnimationState(m_rb.velocity.x);
