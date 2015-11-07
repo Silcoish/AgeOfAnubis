d50a3c44dcc3c896f99e0ee26990c06155a5cda1
Luke Savage <lsava22@hotmail.com>
2015-11-07 11:20:35 +1000

    Spider enemy rewrite
    -Wrote base variables for functionality, wrote awake functions and setup calls.

--- Age of Anubis/Assets/Scripts/Entities/Enemies/ENY_Spider_001.cs
+++ Age of Anubis/Assets/Scripts/Entities/Enemies/ENY_Spider_001.cs
@@ -1,225 +1,147 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public enum Facing { UP, DOWN, LEFT, RIGHT}
-public enum CollisionState { NONE, LEDGE, WALL}
+using System.Collections.Generic;
 
 public class ENY_Spider_001 : Enemy 
 {
     [Header("SpiderStats")]
     public float m_moveSpeed = 2f;
 
-    public float m_gravityScale;
-    public float m_ledgeCheckDist;
+    public Transform m_topRight;
+    public Transform m_topLeft;
+    public Transform m_botRight;
+    public Transform m_botLeft;
+    private Transform m_ledgeCheck;
+    private Transform m_wallCheck;
 
-    public Facing m_facing = Facing.RIGHT;
+    private bool m_isClimbing = true;
+    private bool m_isVertical = true;
+    private Vector2 m_dir;
+    public float m_checkOffset = 0.1F;
+    public float m_checkRadius = 0.1F;
+    public LayerMask m_platformMask;
 
-    public Vector2 m_forward;
-    public Vector2 m_below;
-    public bool m_isInverted = false; // true if on ceiling/right side of wall
-    public CollisionState m_colState = CollisionState.NONE;
 
     public override void AwakeOverride()
     {
         base.AwakeOverride();
 
-        if (Random.value >= 0.5F)
+        // Set up check transforms
+        float offset = GetComponent<CircleCollider2D>().radius + m_checkOffset;
+        m_topRight.localPosition = new Vector2(offset, offset);
+        m_topLeft.localPosition = new Vector2(-offset, offset);
+        m_botRight.localPosition = new Vector2(offset, -offset);
+        m_botLeft.localPosition = new Vector2(-offset, -offset);
+
+        // There should always be 2 colliders active when we awake (ie the platform we are standing on)
+        // Pick one of those 2 as the ledge check, which also determines our initial direction and wall check.
+        List<Transform> ledges = new List<Transform>();
+
+        if (CheckPosition(m_topRight))
+            ledges.Add(m_topRight);
+        if (CheckPosition(m_topLeft))
+            ledges.Add(m_topLeft);
+        if (CheckPosition(m_botRight))
+            ledges.Add(m_botRight);
+        if (CheckPosition(m_botLeft))
+            ledges.Add(m_botLeft);
+
+        if(ledges.Count >= 2)
         {
-            m_facing = Facing.RIGHT;
+            if (Random.value > 0.5)
+                m_ledgeCheck = ledges[0];
+            else
+                m_ledgeCheck = ledges[1];
+
+            if (ledges[0].position.x == ledges[1].position.x)
+                m_isVertical = true;
+            else
+                m_isVertical = false;
+
+            m_wallCheck = GetWallTransform(m_ledgeCheck);
+            EnableClimbing(true);
         }
         else
         {
-            m_facing = Facing.LEFT;
+            // No ledges were in range, we should fall
+            EnableClimbing(false);
         }
-        m_isInverted = false;
-        ChangeDirectionVectors();
+    }
 
-        m_ledgeCheckDist = GetComponent<CircleCollider2D>().radius + 0.1F;
-        m_gravityScale = Physics2D.gravity.y;
+    public override void OnTakeDamage(Damage dam)
+    {
+        base.OnTakeDamage(dam);
 
+        EnableClimbing(false);
     }
 
     public override void EnemyBehaviour()
     {
-        if(CheckPlatformInRange(transform.position, m_forward, m_ledgeCheckDist))
+        if(!m_isClimbing)
         {
-            // Wall in front
-            Debug.Log("Wall in front");
-            m_colState = CollisionState.WALL;
-        }
-        else if(CheckPlatformInRange(transform.position, m_below, m_ledgeCheckDist))
-        {
-            // Moving off of ledge
-            Debug.Log("Ledge reached");
-            m_colState = CollisionState.LEDGE;
+            // Check if we have hit the ground
+            if(m_rb.velocity.y == 0)
+            {
+                // Reenable climbing once we stop falling
+                EnableClimbing(true);
+            }
         }
         else
         {
-            m_colState = CollisionState.NONE;
+            // 
         }
-
-        // HERE WE GO BOYS!
-        switch(m_colState)
-        {
-            case CollisionState.WALL:
-                {
-                    switch(m_facing)
-                    {
-                        case Facing.UP:
-                            {
-                                if(m_isInverted)
-                                {
-                                    m_facing = Facing.RIGHT;
-                                    m_isInverted = true;
-                                }
-                                else
-                                {
-                                    m_facing = Facing.LEFT;
-                                    m_isInverted = true;
-                                }
-                            }
-                            break;
-                        case Facing.DOWN:
-                            {
-                                if (m_isInverted)
-                                {
-                                    m_facing = Facing.LEFT;
-                                    m_isInverted = false;
-                                }
-                                else
-                                {
-                                    m_facing = Facing.RIGHT;
-                                    m_isInverted = false;
-                                }
-                            }
-                            break;
-                        case Facing.LEFT:
-                            {
-                                if (m_isInverted)
-                                {
-                                    m_facing = Facing.DOWN;
-                                    m_isInverted = true;
-                                }
-                                else
-                                {
-                                    m_facing = Facing.UP;
-                                    m_isInverted = true;
-                                }
-                            }
-                            break;
-                        case Facing.RIGHT:
-                            {
-                                if (m_isInverted)
-                                {
-                                    m_facing = Facing.DOWN;
-                                    m_isInverted = false;
-                                }
-                                else
-                                {
-                                    m_facing = Facing.UP;
-                                    m_isInverted = false;
-                                }
-                            }
-                            break;
-                    }
-                }
-                ChangeDirectionVectors();
-                break;
-            case CollisionState.LEDGE:
-                {
-                    switch (m_facing)
-                    {
-                        case Facing.UP:
-                            {
-                                if (m_isInverted)
-                                {
-                                    m_facing = Facing.LEFT;
-                                    m_isInverted = false;
-                                }
-                                else
-                                {
-                                    m_facing = Facing.RIGHT;
-                                    m_isInverted = false;
-                                }
-                            }
-                            break;
-                        case Facing.DOWN:
-                            {
-                                if (m_isInverted)
-                                {
-                                    m_facing = Facing.LEFT;
-                                    m_isInverted = true;
-                                }
-                                else
-                                {
-                                    m_facing = Facing.RIGHT;
-                                    m_isInverted = true;
-                                }
-                            }
-                            break;
-                        case Facing.LEFT:
-                            {
-                                if (m_isInverted)
-                                {
-                                    m_facing = Facing.UP;
-                                    m_isInverted = false;
-                                }
-                                else
-                                {
-                                    m_facing = Facing.DOWN;
-                                    m_isInverted = false;
-                                }
-                            }
-                            break;
-                        case Facing.RIGHT:
-                            {
-                                if (m_isInverted)
-                                {
-                                    m_facing = Facing.UP;
-                                    m_isInverted = true;
-                                }
-                                else
-                                {
-                                    m_facing = Facing.DOWN;
-                                    m_isInverted = true;
-                                }
-                            }
-                            break;
-                    }
-                }
-                ChangeDirectionVectors();
-                break;
-            default:
-                // Haven't reached wall or ledge, keep going.
-                break;
-        }
-
-        m_rb.velocity = (m_forward * m_moveSpeed) + (m_below * Mathf.Abs(m_gravityScale));
-
-        Debug.DrawRay(transform.position, m_forward, Color.yellow);
-        Debug.DrawRay(transform.position, m_below, Color.red);
     }
 
-    void ChangeDirectionVectors()
+    bool CheckPosition(Transform pos)
     {
-        switch(m_facing)
+        return Physics2D.OverlapCircle(pos.position, m_checkRadius, m_platformMask);
+    }
+
+    void EnableClimbing(bool state)
+    {
+        if(state)
         {
-            case Facing.UP:
-                m_forward = new Vector2(0, 1);
-                m_below = m_isInverted ? new Vector2(-1, 0) : new Vector2(1, 0);
-                break;
-            case Facing.DOWN:
-                m_forward = new Vector2(0, -1);
-                m_below = m_isInverted ? new Vector2(-1, 0) : new Vector2(1, 0);
-                break;
-            case Facing.LEFT:
-                m_forward = new Vector2(-1, 0);
-                m_below = m_isInverted ? new Vector2(0, 1) : new Vector2(0, -1);
-                break;
-            case Facing.RIGHT:
-                m_forward = new Vector2(1, 0);
-                m_below = m_isInverted ? new Vector2(0, 1) : new Vector2(0, -1);
-                break;
+            m_isClimbing = true;
+            m_rb.gravityScale = 0;
+            Debug.Log("Climbing - true");
+        }
+        else
+        {
+            m_isClimbing = false;
+            m_rb.gravityScale = 1;
+            Debug.Log("Climbing - false");
+        }
+    }
+
+    Transform GetWallTransform(Transform ledge)
+    {
+        if(ledge == m_topRight)
+        {
+            if (m_isVertical)
+                return m_topLeft;
+            else
+                return m_botRight;
+        }
+        else if(ledge == m_topLeft)
+        {
+            if (m_isVertical)
+                return m_topRight;
+            else
+                return m_botLeft;
+        }
+        else if(ledge == m_botRight)
+        {
+            if (m_isVertical)
+                return m_botLeft;
+            else
+                return m_topRight;
+        }
+        else
+        {
+            if (m_isVertical)
+                return m_botRight;
+            else
+                return m_topLeft;
         }
     }
 }
