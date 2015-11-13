0f825f040ac79468c01a148142be7997e26a34d5
Silcoish <silcoish@gmail.com>
2015-11-13 11:12:34 +1000

    Git chnages

---
+++ Age of Anubis/Assets/Editor/NestedPrefabs.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum EnemyType
+{
+	Scarab,
+	Spider,
+	Snake,
+	Eye
+}
+
+public class NestedPrefabs : MonoBehaviour 
+{
+	public EnemyType m_enemyType;
+	public int m_level;
+
+	GameObject m_prefab;
+
+	public void ResetPrefab()
+	{
+		//m_prefab = Resources.Load(".." + m_prefabLocation) as GameObject;
+
+		EnemyManager em = EnemyManager.Inst;
+
+		if (em == null)
+			Debug.LogError("Enemy Manager Inst Not Found");
+
+		m_prefab = em.GetPrefab(m_enemyType, m_level);
+
+		if (m_prefab != null)
+		{
+			GameObject createdObject = Instantiate(m_prefab, transform.position, transform.rotation) as GameObject;
+
+			createdObject.name = gameObject.name;
+			createdObject.transform.parent = transform.parent;
+			createdObject.transform.position = transform.position;
+			createdObject.transform.rotation = transform.rotation;
+			createdObject.transform.localScale = transform.localScale;
+
+			DestroyImmediate(gameObject);
+		}
+		else
+		{
+			Debug.LogError("Could not find prefab, Please check", gameObject);
+		}
+	}
+
+}
---
+++ Age of Anubis/Assets/Minimap.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minimap : MonoBehaviour {
+
+	void Awake()
+	{
+		GameManager.inst.visibleMap = gameObject;
+	}
+
+}
