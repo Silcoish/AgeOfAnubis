0fc27f7c54282b18ffa83ca50fd9e42b6ed0b1cc
CSchnitzerling <110841@student.sae.edu.au>
2015-11-15 16:37:31 +1000

    Seperated the Enemies and Doors reset PRefab thing

--- Age of Anubis/Assets/Editor/PrefabReset.cs
+++ Age of Anubis/Assets/Editor/PrefabReset.cs
@@ -5,8 +5,8 @@ using System.IO;
 public class PrefabReset : EditorWindow
 {
 
-	[MenuItem("GameObject/PrefabReset")]
-	static void Init()
+    [MenuItem("GameObject/PrefabReset_Enemies")]
+	static void InitEnemies()
 	{
 		NestedPrefabs[] allPrefabs = GameObject.FindObjectsOfType<NestedPrefabs>();
 
@@ -14,7 +14,8 @@ public class PrefabReset : EditorWindow
 
 		foreach (var pf in allPrefabs)
 		{
-			pf.ResetPrefab();
+            if (pf.m_enemyType != EnemyType.Door)
+			    pf.ResetPrefab();
 
 		}
 
@@ -31,6 +32,34 @@ public class PrefabReset : EditorWindow
 
 		Debug.Log("AllPrefabs Reset");
 	}
+
+    [MenuItem("GameObject/PrefabReset_Doors")]
+    static void InitDoors()
+    {
+        NestedPrefabs[] allPrefabs = GameObject.FindObjectsOfType<NestedPrefabs>();
+
+        Debug.Log("Prfabs Found: " + allPrefabs.Length);
+
+        foreach (var pf in allPrefabs)
+        {
+            if (pf.m_enemyType == EnemyType.Door)
+                pf.ResetPrefab();
+
+        }
+
+        GameObject[] allRooms = GameObject.FindGameObjectsWithTag("Room");
+
+        Debug.Log("Rooms Found: " + allRooms.Length);
+
+        foreach (var room in allRooms)
+        {
+            PrefabUtility.ReplacePrefab(room, PrefabUtility.GetPrefabParent(room), ReplacePrefabOptions.ConnectToPrefab);
+        }
+
+
+
+        Debug.Log("AllPrefabs Reset");
+    }
 
 
 	//void OnGUI()
